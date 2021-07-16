98f30eeb164c1f27d2caf1bef4ac4f0fd4d9677a
aditya <aditya003.ay@gmmail.com>
2021-07-16 21:54:31 +0530

    created QueryUtils file to add arraylist

--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -45,7 +45,6 @@ import java.util.ArrayList;
             // Try to parse the SAMPLE_JSON_RESPONSE. If there's a problem with the way the JSON
             // is formatted, a JSONException exception object will be thrown.
             // Catch the exception so the app doesn't crash, and print the error message to the logs.
-            try{
 
                 // TODO: Parse the response given by the SAMPLE_JSON_RESPONSE string and
 
@@ -74,12 +73,7 @@ import java.util.ArrayList;
                     }
 
 
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-
-            } catch (JSONException e) {
+                }  catch (JSONException e) {
                 // If an error is thrown when executing any of the above statements in the "try" block,
                 // catch the exception here, so the app doesn't crash. Print a log message
                 // with the message from the exception.
